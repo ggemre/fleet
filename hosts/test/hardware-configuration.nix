1ac0fb610bc4593018f2f55fa4bdcdcbd420c9ef
ggemre <ggemre+github@proton.me>
2025-04-04 19:22:35 -0600

    feat(test): experimenting with not-detected.nix

--- hosts/test/hardware-configuration.nix
+++ hosts/test/hardware-configuration.nix
@@ -1,10 +1,16 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
+  imports =
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+    
   boot.initrd.availableKernelModules = [ "ahci" "ohci_pci" "ehci_pci" "usb_storage" "sd_mod" "sr_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
+  boot.loader.grub.enable = true;
 
   networking.useDHCP = true;
   networking.interfaces.enp1s0.useDHCP = true;
