5a6112c3ef5d07b44c2d6f568b180d5640ac5731
ggemre <ggemre+github@proton.me>
2025-04-13 21:30:24 -0600

    feat(home): anyrun styles

--- modules/home/graphical/anyrun.nix
+++ modules/home/graphical/anyrun.nix
@@ -1,6 +1,7 @@
 {
   anyrun,
   pkgs,
+  config,
   ...
 }: {
   programs.anyrun = {
@@ -20,7 +21,7 @@
       x = {fraction = 0.5;};
       y = {fraction = 0.3;};
       width = {fraction = 0.3;};
-      layer = "overlay";
+      layer = "top";
       hideIcons = true;
       hidePluginInfo = true;
       maxEntries = 15;
@@ -28,5 +29,50 @@
       ignoreExclusiveZones = false;
       closeOnClick = true;
     };
+
+    extraCss = ''
+      @define-color text #${config.lib.stylix.colors.base05};
+      @define-color alt-text #${config.lib.stylix.colors.base04};
+      @define-color bg-active #${config.lib.stylix.colors.base01};
+      @define-color bg-inactive #${config.lib.stylix.colors.base00};
+      @define-color unselected #${config.lib.stylix.colors.base00};
+      @define-color selected #${config.lib.stylix.colors.base03};
+
+      * {
+        transition: 200ms ease;
+        font-family: "${config.stylix.fonts.monospace.name}";
+        font-size: 12px;
+        font-weight: bold;
+      }
+
+      #main,
+      #plugin {
+        color: @text;
+        background-color: @bg-active;
+        border: 1px solid @bg-inactive;
+        border-radius: 4px;
+      }
+
+      #window {
+        background-color: rgba(43, 48, 59, 0.5);
+      }
+
+      #entry {
+        color: @text;
+        background-color: @bg-inactive;
+        border-radius: 4px;
+      }
+
+      #match {
+        color: @text;
+        background: @unselected;
+        padding: 4px;
+      }
+
+      #match:selected {
+        color: @alt-text;
+        background-color: @selected;
+      }
+    '';
   };
 }
